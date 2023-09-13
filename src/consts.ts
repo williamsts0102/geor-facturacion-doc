18fabaa25b7328fc03a2f3c52a8da3d8b4a3b360
Yoel Cerna Torres <joel_20_10@hotmail.com>
2023-09-13 12:24:17 -0500

    first commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Geor Ecommerce Docs';
+export const SITE_DESCRIPTION = 'Documentación del proyecto ecommerce Renac';
